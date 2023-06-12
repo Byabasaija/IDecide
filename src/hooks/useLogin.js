71d1f472adb16c110c3f0ec7309154d8a8876b6f
pascal <pascal@marascientific.org>
2023-06-12 18:38:08 +0300

    fix bugs

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -7,7 +7,7 @@ export const useLogin = () => {
 
   const projectID = 'IDecideVotingDapp.myapp.in';
   const scope = 'full';
-  const redirectURL = 'http://localhost:3000/';
+  const redirectURL = 'https://i-decide.vercel.app/';
 
 
   const handleLogin = (e) => {
