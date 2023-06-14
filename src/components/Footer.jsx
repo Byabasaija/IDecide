172ca7c5ce7c133c3a2a2aef6bea71b36ef7cf08
pascal <pascal@marascientific.org>
2023-06-14 18:45:12 +0300

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@
 const Footer = () => {
   return (
  
-<footer class="bg-white shadow dark:bg-gray-900">
-    <div class="w-full max-w-screen-xl mx-auto md:py-4">
+<footer className="bg-white shadow dark:bg-gray-900">
+    <div className="w-full max-w-screen-xl mx-auto md:py-4">
        
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 " />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">IDecide™</a>. All Rights Reserved.</span>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 " />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" class="hover:underline">IDecide™</a>. All Rights Reserved.</span>
     </div>
 </footer>
 
